bca97fe5f70b2e99809ceb6c5947cc807bc09db7
Armando Leon <ajlelorriaga@gmail.com>
2013-04-23 21:11:43 -0400

    Fixed bugs and improved functionality of service (LocationNotificationService)

--- src/com/cis6930/geotaskit/service/LocationNotificationService.java
+++ src/com/cis6930/geotaskit/service/LocationNotificationService.java
@@ -1,10 +1,16 @@
 package com.cis6930.geotaskit.service;
 
+import java.util.List;
+
+import android.app.Activity;
+import android.app.AlarmManager;
 import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.app.Service;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.location.Criteria;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
@@ -13,136 +19,221 @@ import android.os.IBinder;
 import android.preference.PreferenceManager;
 import android.provider.Settings;
 import android.support.v4.app.NotificationCompat;
+import android.support.v4.app.TaskStackBuilder;
 import android.util.Log;
+import android.widget.Toast;
 
 import com.cis6930.geotaskit.MainActivity;
 import com.cis6930.geotaskit.PopupActivity;
 import com.cis6930.geotaskit.R;
+import com.cis6930.geotaskit.Task;
+import com.cis6930.geotaskit.backends.DatabaseInterface;
 
 public class LocationNotificationService extends Service {
-  // private int MINUTES = 1000 * 60 * 5; // (1 sec in milliseconds) * (1 minute
-  // in seconds) * (how many minutes)
-  private int METERS = 200;
-  private int NOTIFICATION_ID = 1;
-  private LocationManager locationManager;
-  private LocationListener loc_listener;
 
-  @Override
-  public IBinder onBind(Intent arg0) {
-    return null;
-  }
+	private int METERS_REMINDER = 1000;
+	private int NOTIFICATION_ID = 1;
+	private LocationManager locationManager;
+	private LocationListener loc_listener;
 
-  @Override
-  public int onStartCommand(Intent intent, int flags, int startId) {
-    // Location listener that will be called whenever the LocationManager
-    // returns a request of updated location
-    loc_listener = new LocationListener() {
-      @Override
-      public void onStatusChanged(String provider, int status, Bundle extras) {
-      }
+	@Override
+	public IBinder onBind(Intent arg0) {
+		return null;
+	}
 
-      @Override
-      public void onProviderEnabled(String provider) {
-      }
+	@Override
+	public int onStartCommand(Intent intent, int flags, int startId) {
+		// Location listener that will be called whenever the LocationManager
+		// returns a request of updated location
+		loc_listener = new LocationListener() {
+			@Override
+			public void onStatusChanged(String provider, int status, Bundle extras) {
+			}
 
-      @Override
-      public void onProviderDisabled(String provider) {
-      }
+			@Override
+			public void onProviderEnabled(String provider) {
+			}
 
-      @Override
-      public void onLocationChanged(Location location) {
-        Log.i("#info", "Loc ( " + location.getLatitude() + " , " + location.getLongitude() + " )");
-        compareToTasks(location);
-      }
-    };
-    /* START REQUESTING LOCATION UPDATES */
-    // This verification should be done during onStart() because the system
-    // calls
-    // this method when the user returns to the activity, which ensures the
-    // desired
-    // location provider is enabled each time the activity resumes from the
-    // stopped state.
-    locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
-    final boolean gpsEnabled = locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
-    // if the gps is disabled by settings then notify the user and prompt the
-    // settings/location screen
-    if (!gpsEnabled) {
-      Log.e("#info", "gps is disabled");
-      enableLocationSettings();
-    }
-    else {
-      SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
-      int minutes = Integer.parseInt(prefs.getString(getString(R.string.pref_user_frequency_key),
-          getString(R.string.pref_user_frequency_default)));
-      locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, minutes * 60 * 1000,
-          METERS, loc_listener);
-    }
-    // return STICKY to maintain the service running
-    return Service.START_STICKY;
-  }
+			@Override
+			public void onProviderDisabled(String provider) {
+			}
 
-  private void enableLocationSettings() {
-    Intent settingsIntent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
-    startActivity(settingsIntent);
-  }
+			@Override
+			public void onLocationChanged(Location location) {
+				// Once got the location stop tracking location
+				locationManager.removeUpdates(loc_listener);
 
-  private void compareToTasks(Location location) {
-    // Get stored tasks from the local database that are inside the notification
-    // radius
-    // Compare to Location argument
-    // if within reminder distance
-    // if single task
-    // title set as "Nearby:" + task.name
-    // body set as task.description
-    // else
-    // title set as # + "pending tasks nearby"
-    // body: for(t:tasks){ body += ", "+ task.name }
-    // create/update notification OR pop-up dialog (DEPENDS ON THE PROPERTY TYPE
-    // OF ALERT)
-    // --------------- AFTER THIS, A NOTIFICATION IS DISPLAYED/UPDATED (WHENEVER
-    // SET BY THE USER) Delete this comment after following above struture
-    // create dummy notification
-    NotificationCompat.Builder notif = new NotificationCompat.Builder(this);
-    // set basic elements to the builder
-    notif.setSmallIcon(R.drawable.notif);
-    notif.setContentTitle("# Task(s) nearby!");
-    notif.setContentText("Buy things, Do stuff, Sleep, Eat, Whatever, Damn you Jane");
-    notif.setTicker("GeoTaskIt - # Task(s) nearby!");
-    notif.setAutoCancel(true);
-    // Creates an explicit intent for an Activity in your app
-    Intent resultIntent = new Intent(this, MainActivity.class);
-    // The stack builder object will contain an artificial back stack for the
-    // started Activity.
-    // This ensures that navigating backward from the Activity leads out of
-    // your application to the Home screen.
-    // TaskStackBuilder stackBuilder = TaskStackBuilder.create(this);
-    // Adds the back stack for the Intent (but not the Intent itself)
-    // stackBuilder.addParentStack(MainActivity.class);
-    // Adds the Intent that starts the Activity to the top of the stack
-    // stackBuilder.addNextIntent(resultIntent);
-    /*
-     * PendingIntent resultPendingIntent = stackBuilder.getPendingIntent( 0, PendingIntent.FLAG_UPDATE_CURRENT );
-     */
-    // set the activity that will be called when notification is clicked
-    // notif.setContentIntent(resultPendingIntent);
-    NotificationManager mNotificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
-    // create the notification (or update with the id)
-    // mNotificationManager.notify(NOTIFICATION_ID, notif.build());
-    // --------------- AFTER THIS, THE POP-UP DIALOG IS DISPLAYED (WHENEVER SET
-    // BY THE USER)
-    // create and call the pop-up reminder (dialog-themed activity) for
-    // important tasks
-    Intent i = new Intent(this, PopupActivity.class);
-    i.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_MULTIPLE_TASK);
-    startActivity(i);
-  }
+				Log.i("#info", "Loc ( " + location.getLatitude() + " , " + location.getLongitude() + " )");
 
-  @Override
-  public void onDestroy() {
-    // if destroy by the system then stop requesting location updates (it will
-    // be restored once the service is restarted when possible)
-    /* STOP REQUESTING LOCATION UPDATES */
-    locationManager.removeUpdates(loc_listener);
-    super.onDestroy();
-  }
+				// Compare tasks with location obtained
+				compareToTasks(location);
+
+				// Reschedule service to run as set by the user
+				rescheduleCheck();
+
+				// Now this service can end
+				stopSelf();
+			}
+		};
+		/* START REQUESTING LOCATION UPDATES */
+		// This verification should be done during onStart() because the system
+		// calls
+		// this method when the user returns to the activity, which ensures the
+		// desired
+		// location provider is enabled each time the activity resumes from the
+		// stopped state.
+
+		locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+
+		// Set criteria with fine accuracy and helping variables to obtain and use providers
+		boolean isCapable = true;
+		String provider = null;
+		Criteria criteria = new Criteria();
+		criteria.setAccuracy(Criteria.ACCURACY_FINE);
+		List<String> list_providers = null;
+
+		// Get all enabled providers with fine accuracy
+		list_providers = locationManager.getProviders(criteria, true);
+
+		/* If no provider was found then try with no accuracy requirement and if the same case, set as not capable via isCapable flag
+		 * Nothing to do if there are no providers, notify user
+		 */
+		if(list_providers == null){
+			criteria.setAccuracy(Criteria.NO_REQUIREMENT);
+			list_providers = locationManager.getProviders(criteria, true);
+			if(list_providers == null)
+				isCapable = false;
+		}
+
+
+		// If is capable
+		if(isCapable){
+
+			// Select first (or only) provider 
+			provider = list_providers.get(0);
+
+			// Get location with selected [enabled] provider (once location is received in listener, stop updates, reschedule service, and kill service
+			locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, loc_listener);
+
+		}else{
+			// Notify the user there are no means to get location, suggest enabling (try to open activity), and end service
+			Toast.makeText(LocationNotificationService.this, getString(R.string.error_location_providers_not_found), Toast.LENGTH_LONG).show();
+			enableLocationSettings();
+			stopSelf();
+		}
+
+		// return STICKY to maintain the service running
+		return Service.START_STICKY;
+	}
+
+	private void rescheduleCheck(){
+
+		// Obtain the preferences of the user on how frequent to check for the location
+		SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+		int minutes = Integer.parseInt(prefs.getString(getString(R.string.pref_user_frequency_key), getString(R.string.pref_user_frequency_default)));
+		long now = System.currentTimeMillis();
+		
+		// Set the intent that will start the service
+		Intent intent = new Intent(LocationNotificationService.this, LocationNotificationService.class);
+		PendingIntent pi = PendingIntent.getService(LocationNotificationService.this, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+
+		// Set through the AlarmManager to start previously-set intent in the stated period + current time
+		AlarmManager am = (AlarmManager) getSystemService(Activity.ALARM_SERVICE);
+		am.set(AlarmManager.RTC_WAKEUP, now + (minutes * 60 * 1000), pi);
+	}
+
+	private void enableLocationSettings() {
+		try{
+			Intent settingsIntent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+			startActivity(settingsIntent);
+		}catch(Exception e){
+			e.printStackTrace();
+		}
+	}
+
+	private void compareToTasks(Location location) {
+		// Get stored tasks from the local database that are inside the notification
+		DatabaseInterface db = new DatabaseInterface(LocationNotificationService.this);
+		List<Task> list_tasks = db.getTasks();
+		
+		
+		String notification_message = null;
+		
+		int counter_nearby = 0;
+		// Compare distance and tasks
+		for(Task task : list_tasks){
+			
+			// Get distance between current location and task
+			float[] results = new float[3];
+			Location.distanceBetween(task.latitude, task.longitude, location.getLatitude(), location.getLongitude(), results);
+			
+			System.out.println("Distance with "+task.name+": "+results[0]);
+			
+			// If task is within distance of reminder, then add to String of reminders to the notification
+			if(results[0] <= METERS_REMINDER){
+				counter_nearby++;
+				
+				if(notification_message == null)
+					notification_message = task.name;
+				else
+					notification_message += ", " + task.name;
+			}
+		}
+		
+		// If there are no tasks nearby then return
+		if(counter_nearby < 0)
+			return;
+		
+		// Set title of notification
+		String notification_title = counter_nearby + " task(s) nearby";
+
+		// create/update notification OR pop-up dialog (DEPENDS ON THE PROPERTY TYPE
+		// OF ALERT)
+		
+		NotificationCompat.Builder notif = new NotificationCompat.Builder(this);
+		// set basic elements to the builder
+		notif.setSmallIcon(R.drawable.notif);
+		notif.setContentTitle(notification_title);
+		notif.setContentText(notification_message);
+		notif.setTicker("GeoTaskIt: "+notification_title);
+		notif.setAutoCancel(true);
+		// Creates an explicit intent for an Activity in your app
+		Intent resultIntent = new Intent(this, MainActivity.class);
+		// The stack builder object will contain an artificial back stack for the
+		// started Activity.
+		// This ensures that navigating backward from the Activity leads out of
+		// your application to the Home screen.
+		TaskStackBuilder stackBuilder = TaskStackBuilder.create(this);
+		// Adds the back stack for the Intent (but not the Intent itself)
+		stackBuilder.addParentStack(MainActivity.class);
+		// Adds the Intent that starts the Activity to the top of the stack
+		stackBuilder.addNextIntent(resultIntent);
+		
+		PendingIntent resultPendingIntent = stackBuilder.getPendingIntent( 0, PendingIntent.FLAG_UPDATE_CURRENT );
+		
+		// set the activity that will be called when notification is clicked
+		notif.setContentIntent(resultPendingIntent);
+		
+		NotificationManager mNotificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+		mNotificationManager.notify(NOTIFICATION_ID, notif.build());
+
+		// --------------- AFTER THIS, THE POP-UP DIALOG IS DISPLAYED (WHENEVER SET
+		// BY THE USER)
+		// create and call the pop-up reminder (dialog-themed activity) for
+		// important tasks
+		/*
+		Intent i = new Intent(this, PopupActivity.class);
+		i.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_MULTIPLE_TASK);
+		startActivity(i);
+		*/
+	}
+
+	@Override
+	public void onDestroy() {
+		// if destroy by the system then stop requesting location updates (it will
+		// be restored once the service is restarted when possible)
+		/* STOP REQUESTING LOCATION UPDATES */
+		locationManager.removeUpdates(loc_listener);
+		super.onDestroy();
+	}
 }
